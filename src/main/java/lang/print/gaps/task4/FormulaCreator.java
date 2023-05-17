f444552a31943b5d2e44aad443a9f362cfe3b9f1
JustDevWeb <zm91672@gmail.com>
2023-05-17 08:22:21 +0800

    dev: task 4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 10;
+        byte b = 5;
+
+       double formula = (9 * Math.pow(a, 2) - 5*b + 2*a -7)* ((a+b - 4*a*b)/2);
+
     }
 }
